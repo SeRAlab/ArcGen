5ac524e606988242ed131472437e2a2f235922f9
Ashlars <yangzhonghao@vip.qq.com>
2025-08-08 21:09:52 +0800

    Fix the path issue

--- defence/utils/__init__.py
+++ defence/utils/__init__.py
@@ -2,7 +2,7 @@
 Author: Ashlars yangzhonghao@vip.qq.com
 Date: 2025-07-14 22:14:24
 LastEditors: Ashlars yangzhonghao@vip.qq.com
-LastEditTime: 2025-07-14 22:14:48
+LastEditTime: 2025-08-08 21:09:30
 FilePath: /ArcGen/defence/utils/__init__.py
 '''
-from network_dataset import *
+from defence.utils.network_dataset import *
